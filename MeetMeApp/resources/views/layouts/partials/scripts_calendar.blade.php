77e9ec4e3e34b78ad9ede03d547ca2b3718be1e8
Daniel Mendez Cruz <daniboymendez@gmail.com>
2016-04-27 23:52:56 -0500

    agregando proyecto MeetMe

---
+++ MeetMeApp/resources/views/layouts/partials/scripts_calendar.blade.php
@@ -0,0 +1,23 @@
+<!-- REQUIRED JS SCRIPTS -->
+
+<!-- jQuery 2.1.4 -->
+<script src="{{ asset('/plugins/jQuery/jQuery-2.1.4.min.js') }}"></script>
+<!-- Bootstrap 3.3.2 JS -->
+<script src="{{ asset('/js/bootstrap.min.js') }}" type="text/javascript"></script>
+<!-- AdminLTE App -->
+<script src="{{ asset('/js/app.min.js') }}" type="text/javascript"></script>
+
+<!-- jQuery UI 1.11.4 -->
+    <script src="https://code.jquery.com/ui/1.11.4/jquery-ui.min.js"></script>
+
+<!-- Slimscroll -->
+    <script src="{{asset('/plugins/slimScroll/jquery.slimscroll.min.js')}}" type="text/javascript"></script>
+    <!-- FastClick -->
+    <script src="{{asset('/plugins/fastclick/fastclick.min.js')}}" type="text/javascript"></script>
+    <!-- fullCalendar 2.2.5 -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.10.2/moment.min.js"></script>
+    <script src="{{asset('/plugins/fullcalendar/fullcalendar.min.js')}}" type="text/javascript"></script>
+<!-- Optionally, you can add Slimscroll and FastClick plugins.
+      Both of these plugins are recommended to enhance the
+      user experience. Slimscroll is required when using the
+      fixed layout. -->
